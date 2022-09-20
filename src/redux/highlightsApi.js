0b7548434413b3d6cdd9a0a0c91e913ff8df4020
Thermiee <navyblack16@yahoo.com>
2022-09-20 21:29:39 +0100

    add video api

--- src/redux/highlightsApi.js
+++ src/redux/highlightsApi.js
@@ -1,6 +1,6 @@
 const fetchHighlightsApi = async (title) => {
   try {
-    const api = await fetch(`https://www.scorebat.com//${title}//`);
+    const api = await fetch(`https://scorebat.com/video/${title}`);
     const response = await api.json();
     return response;
   } catch (err) {
